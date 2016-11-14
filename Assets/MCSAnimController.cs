eb69e964348a05efd6cfa9ebbde68426b0fdf248
Dave Yan <fdu5526@gmail.com>
2016-11-14 15:33:42 -0800

    added  human thing

---
+++ Assets/MCSAnimController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MCSAnimController : MonoBehaviour {
+
+	private Animator anim;
+	private float run;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		run = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		run = Input.GetAxis("Horizontal");
+		anim.SetFloat ("run", run);
+	}
+}
