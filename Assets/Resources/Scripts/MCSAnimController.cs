43967229490e80a79e7e84ad2d7f1fc2ea690ac8
Dave Yan <fdu5526@gmail.com>
2016-11-14 18:22:42 -0800

    added all clips to person animation controller

---
+++ Assets/Resources/Scripts/MCSAnimController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MCSAnimController : MonoBehaviour {
+
+	private Animator anim;
+	private float run;
+
+
+	public enum AnimationState { Idle, Interacting, Running, Sprinting, Turn90, Turn180 };
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		run = 0.0f;
+	}
+
+
+	public void SetAnimatorState (AnimationState state) {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		run = Input.GetAxis("Horizontal");
+		anim.SetFloat ("run", run);
+	}
+}
---
+++ Assets/Resources/Scripts/SceneMCSAnimController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneMCSAnimController : MonoBehaviour {
+
+	MCSAnimController personController;
+
+	// Use this for initialization
+	void Start () {
+		personController = FindObjectOfType<MCSAnimController>();
+	}
+
+
+	public void SetAnimatorState (MCSAnimController.AnimationState state) {
+		personController.SetAnimatorState(state);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
